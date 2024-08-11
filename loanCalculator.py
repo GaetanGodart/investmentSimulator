9c0d7e7add9119f9a659af90e4b68f5d08928bf2
Gaetan <gaetankelabra@gmail.com>
2024-08-11 22:30:44 +0200

    Fix typo

--- loanCalculator.py
+++ loanCalculator.py
@@ -5,11 +5,11 @@ def calculateLoan(capital, duration, percentageFeeYearly, flatFeeMontly):
     payedBack = np.array([0])
     remaining = np.array([capital])
 
-    montlyPayment = (percentageFeeYearly/12*capital) / (1-(1/pow(1+percentageFeeYearly/12, duration*12))) + flatFeeMontly
+    monthlyPayment = (percentageFeeYearly/12*capital) / (1-(1/pow(1+percentageFeeYearly/12, duration*12))) + flatFeeMontly
 
     for i in range(duration*12+1):
             costOfLoan = np.append(costOfLoan, costOfLoan[i] + remaining[i]*percentageFeeYearly/12 + flatFeeMontly*12)
-            payedBack = np.append(payedBack, payedBack[i] + montlyPayment - remaining[i]*percentageFeeYearly/12 - flatFeeMontly)
-            remaining = np.append(remaining, remaining[i] - montlyPayment + remaining[i]*percentageFeeYearly/12 + flatFeeMontly)
+            payedBack = np.append(payedBack, payedBack[i] + monthlyPayment - remaining[i]*percentageFeeYearly/12 - flatFeeMontly)
+            remaining = np.append(remaining, remaining[i] - monthlyPayment + remaining[i]*percentageFeeYearly/12 + flatFeeMontly)
 
-    return montlyPayment, costOfLoan, payedBack, remaining
+    return monthlyPayment, costOfLoan, payedBack, remaining
